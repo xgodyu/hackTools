7096562f71a8b33eed627f8cc6b0ebc844440f9c
Godyu <108613516+xgodyu@users.noreply.github.com>
2024-05-10 20:41:29 +0800

    Add files via upload

---
+++ 一键启动.bat
@@ -0,0 +1,5 @@
+@echo off
+pushd %~dp0
+.\javafx-sdk-22\jdk-20\bin\java -jar shenyu.jar
+popd
+pause
---
+++ 一键安装requestments.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Installing required Python packages from requirements.txt...
+pip install -r requirements.txt
+echo Done.
+pause
